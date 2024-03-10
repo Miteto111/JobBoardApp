4ca3b38e9c1b3af4998cd16391a9996bdff5be08
User <User@DESKTOP-DAGVMIP>
2024-03-10 11:06:45 +0200

    Add project files.

---
+++ JobBoardApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobBoardApp.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
